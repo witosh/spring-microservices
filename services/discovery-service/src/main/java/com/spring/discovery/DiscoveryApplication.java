4c44723f24e04a4ff4ffaf2ce9d8b1f541557f35
Witold <witold.sobczakk@gmail.com>
2019-03-12 23:46:33 +0100

    [Services] discovery-service gets remote configuration

--- services/discovery-service/src/main/java/com/spring/discovery/DiscoveryApplication.java
+++ services/discovery-service/src/main/java/com/spring/discovery/DiscoveryApplication.java
@@ -1,9 +1,8 @@
-package com.spring.discovery.discovery;
+package com.spring.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.EnableMBeanExport;
 
 @SpringBootApplication
 @EnableEurekaServer

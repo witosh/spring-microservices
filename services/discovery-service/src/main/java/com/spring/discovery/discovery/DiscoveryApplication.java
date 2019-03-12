e19cd3144da1889b0b3187812357a71aa978977e
Witold <witold.sobczakk@gmail.com>
2019-03-12 20:15:31 +0100

    [Services] Add config-services= and discovery-service

---
+++ services/discovery-service/src/main/java/com/spring/discovery/discovery/DiscoveryApplication.java
@@ -0,0 +1,16 @@
+package com.spring.discovery.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.EnableMBeanExport;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryApplication.class, args);
+	}
+
+}

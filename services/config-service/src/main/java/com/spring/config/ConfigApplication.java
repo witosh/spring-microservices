bcaaac8cde86f8cabcbbb66be73421cf8594d89d
wsobczak <witold.sobczak@comarch.com>
2019-05-23 17:49:00 +0200

    Problem with healthcheck

--- services/config-service/src/main/java/com/spring/config/ConfigApplication.java
+++ services/config-service/src/main/java/com/spring/config/ConfigApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
 @EnableConfigServer
+@SpringBootApplication
 public class ConfigApplication {
 
 	public static void main(String[] args) {
